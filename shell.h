d18e566d2d8bad09feddbd950f55e7580a205b7c
Skander <62188738+skanderdridi@users.noreply.github.com>
2023-07-15 00:01:41 +0100

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,24 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+extern char **environ;
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stddef.h>
+#include <wait.h>
+#include <stddef.h>
+#include <errno.h>
+#include <dirent.h>
+
+
+#endif
