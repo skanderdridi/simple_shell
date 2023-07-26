497b3c61a0fc005b5ca44580b008f62c8a1646c3
Skander <62188738+skanderdridi@users.noreply.github.com>
2023-07-26 21:45:49 +0100

    Update get_path.c

--- get_path.c
+++ get_path.c
@@ -1,31 +1,11 @@
 #include "shell.h"
-#include <stdio.h>
+
 /**
- * get_path - function that gets the path
- * @cmd: the command line
+ * get_path - Returns the value of the PATH enviroment variable.
+ *
+ * Return: Pointer to the value of $PATH.
  */
-
-void get_path(char **cmd)
+char *get_path(void)
 {
-	int i;
-	char **tok = NULL, *path = strdup(getenv("PATH")), *tmp = NULL;
-
-	tok = str_split(path, ":");
-	free(path);
-	path = NULL;
-	for (i = 0; tok[i]; i++)
-	{
-		tmp = calloc(sizeof(char), (strlen(tok[i]) + 1 + strlen(cmd[0]) + 1));
-		strcat(tmp, tok[i]);
-		strcat(tmp, "/");
-		strcat(tmp, cmd[0]);
-
-		if (access(tmp, F_OK) == 0)
-			break;
-		free(tmp);
-		tmp = NULL;
-	}
-	free_array(tok);
-	free(cmd[0]);
-	cmd[0] = tmp;
+	return (print_environment("PATH"));
 }
