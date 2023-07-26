d014c7998d5e2c00bd41eea2b6f61807fabd6a08
Skander <62188738+skanderdridi@users.noreply.github.com>
2023-07-26 08:33:05 +0100

    Update path.c

--- path.c
+++ path.c
@@ -9,78 +9,30 @@
  *
  * Return: 1 (not absolute) 0 (absolute)
  **/
-int _path(char *path)
+char *path(char *filename)
 {
-	if (strlen(path) > 3)
-	{
-		if ((path[0] == '.' && path[1] == '/') || path[0] == '/' ||
-			(path[0] == '.' && path[1] == '.' && path[2] == '/'))
-			return (1);
-	}
-
-	return (0);
-}
-
-/**
- * make_path - make path to file from directory and file
- * @path: path to directory
- * @file: file
- * Return: New Path
- **/
-char *make_path(char *path, char *file)
-{
-	char *n;
-
-	if (path == NULL || file == NULL)
-		return (NULL);
-
-	n = malloc(sizeof(char) *
-		(strlen(path) + strlen(file) + 2));
-	if (!n)
-		return (NULL);
-
-	strcpy(n, path);
-	n[strlen(path)] = '/';
-	n[strlen(path) + 1] = '\0';
-	strcat(n, file);
-
-	return (n);
-}
-
-/**
- * _match - find directory insid PATH
- * that file resides in if any
- * @exec: executable name
- * Return: full path of executable or NULL if it's not found
- **/
-char *_match(char **exec)
-{
-	char **array, *p, *path;
-	int i = 0;
+	char *PATH = getenv("PATH");
+	char *cpy = _strdup(PATH), *concatenated = NULL;
+	char *token = NULL, *absolute = NULL;
 	struct stat st;
 
-	path = getenv("PATH");
-	if (strlen(path) == 0)
-		return (NULL);
-
-	array = str_split(path, ':');
-	if (!array)
-		return (NULL);
-
-	while (array[i] != NULL)
+	token = _strtok(cpy, ':');
+	concatenated = str_concat("/", filename);
+	while (token != NULL)
 	{
-		p = make_path(array[i], *exec);
-		if (stat(p, &st) == 0)
+		absolute = str_concat(token, concatenated);
+		if (stat(absolute, &st) == 0)
 		{
-			free(*exec);
-			*exec = p;
-			free_arr(array);
-			return (p);
+			free(PATH);
+			free(cpy);
+			free(concatenated);
+			return (absolute);
 		}
-
-		free(p);
-		i++;
+		token = _strtok(NULL, ':');
+		free(absolute);
 	}
-	free_arr(array);
+	free(PATH);
+	free(concatenated);
+	free(cpy);
 	return (NULL);
 }
