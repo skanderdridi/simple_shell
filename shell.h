7c99555e8266a37fdfff7a8ad2ff2957eb5942aa
Skander <62188738+skanderdridi@users.noreply.github.com>
2023-07-26 22:24:02 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,25 +1,28 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/*LIBRARIES*/
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 
-extern char **environ;
-
-char **str_split(char *line, char *delim);
-void free_array(char **array);
-void loop(int input);
-void get_path(char **cmd);
-void exec_command(char **cmd);
-void print_environment(void);
-void sigintHandler(int sig_num);
-void if_conditions(char **cmd, char *line);
+/*PROTOTYPES*/
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
 char *search_path(char *command);
+char **_split(char *str, char *sep);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *_getenv(char *env_var);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void _env(void);
+int execute(char **args);
+int empty_line(char *buff);
+
+/* GLOBAL VAR*/
+extern char **environ;
 
 #endif
