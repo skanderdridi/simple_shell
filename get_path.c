ce8ccda5d87ee950b93f4d797b7b0f52e0674eef
Skander <62188738+skanderdridi@users.noreply.github.com>
2023-07-26 21:56:09 +0100

    Update get_path.c

--- get_path.c
+++ get_path.c
@@ -1,72 +1,31 @@
 #include "shell.h"
-
+#include <stdio.h>
 /**
- * checkPath - searches $PATH for directory of command
- *
- * Description: checks the path of a given command
- * @build: input build
- * Return: the state of the path values
+ * get_path - function that gets the path
+ * @cmd: the command line
  */
-_Bool check_path(config *build)
+
+void get_path(char **cmd)
 {
-	register int len;
-	static char buffer[BUFSIZE];
-	struct stat st;
-	char *token, *copy, *delim = ":", *tmp;
-	_Bool inLoop = false;
+	int i;
+	char **tok = NULL, *path = strdup(getenv("PATH")), *tmp = NULL;
 
-	if (validate_constraints(build))
-		return (true);
-	copy = _strdup(build->path);
-	token = _strtok(copy, delim);
-	while (token)
+	tok = str_split(path, ":");
+	free(path);
+	path = NULL;
+	for (i = 0; tok[i]; i++)
 	{
-		tmp = inLoop ? token - 2 : token;
-		if (*tmp == 0 && stat(build->args[0], &st) == 0)
-		{
-			build->full_path = build->args[0];
-			return (true);
-		}
-		len = _strlen(token) + _strlen(build->args[0]) + 2;
-		_strcat(buffer, token);
-		_strcat(buffer, "/");
-		_strcat(buffer, build->args[0]);
-		get_null_bytes(buffer, len - 1);
-		if (stat(buffer, &st) == 0)
-		{
-			build->full_path = buffer;
-			free(copy);
-			return (true);
-		}
-		get_null_bytes(buffer, 0);
-		token = _strtok(NULL, delim);
-		inLoop = true;
-	}
-	build->full_path = build->args[0];
-	return (false);
-}
+		tmp = calloc(sizeof(char), (strlen(tok[i]) + 1 + strlen(cmd[0]) + 1));
+		strcat(tmp, tok[i]);
+		strcat(tmp, "/");
+		strcat(tmp, cmd[0]);
 
-/**
- * checkEdgeCases - helper func for check path to check edge cases
- * @build: input build
- * Return: true if found, false if not
- */
-_Bool validate_constraints(config *build)
-{
-	char *copy;
-	struct stat st;
-
-	copy = _strdup(build->path);
-	if (!copy)
-	{
-		build->full_path = build->args[0];
-		return (true);
+		if (access(tmp, F_OK) == 0)
+			break;
+		free(tmp);
+		tmp = NULL;
 	}
-	if (*copy == ':' && stat(build->args[0], &st) == 0)
-	{
-		build->full_path = build->args[0];
-		return (true);
-	}
-	free(copy);
-	return (false);
+	free_array(tok);
+	free(cmd[0]);
+	cmd[0] = tmp;
 }
